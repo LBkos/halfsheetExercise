31a6d9453fd9c855c8dc676f498b4bb3e0ffd558
LBkos <constantin.l.o.p@gmail.com>
2023-07-11 17:52:36 +0700

    Initial Commit

---
+++ halfSheet/halfSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  halfSheetApp.swift
+//  halfSheet
+//
+//  Created by Константин Лопаткин on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct halfSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
